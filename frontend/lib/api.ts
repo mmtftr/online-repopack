0bd6b343b895f470e6cd013f1f8108e8f24eee75
Mehmet Efe Akça <efeakca@gmail.com>
2024-10-23 16:43:01 +0300

    fix env

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,7 @@ import Client, { Environment, Local } from "@/app/lib/client";
 import { RepopackRequest, RepopackResponse } from "./types";
 
 
-const API_BASE_URL = process.env.NODE_ENV === "development" ? Local : Environment(process.env.NODE_ENV);
+const API_BASE_URL = process.env.NODE_ENV === "development" ? Local : Environment("staging");
 
 const cl = new Client(API_BASE_URL);
 
